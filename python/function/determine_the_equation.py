fb5401c97f24747e37849a9c4970347529d5ce61
AmmanRizwan <ammanrizwan00007@gmail.com>
2025-07-26 18:41:09 +0530

    Create a function determine the equation that will equalize the formula is done

---
+++ python/function/determine_the_equation.py
@@ -0,0 +1,27 @@
+"""
+Write a short program that takes as input three integers, a, b, and c, from
+the console and determines if they can be used in a correct arithmetic
+formula (in the given order), like “a + b = c,” “a = b − c,” or “a ∗ b = c.”
+"""
+
+def determine_the_equation() -> bool:
+    try:
+        correct_arith: bool | None = None
+        a = int(input("Enter a integer for a: "))
+        b = int(input("Enter a integer for b: "))
+        c = int(input("Enter a integer for c: "))
+
+        if a + b == c:
+            correct_arith = True
+        elif b - c == a:
+            correct_arith = True
+        elif a * b == c:
+            correct_arith = True
+        else:
+            correct_arith = False
+        return correct_arith
+
+    except ValueError:
+        print("Please Enter a Valid Integer")
+
+determine_the_equation()
